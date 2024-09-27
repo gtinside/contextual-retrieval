940ff7c0e3019ebb203533c912b4ad5ed70061a4
Gaurav Tiwari <gauravtiwari01@gmail.com>
2024-09-26 23:35:01 -0400

    Added dependencies

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import anthropic
 import chromadb
 from dotenv import load_dotenv
-from llama_index.core import StorageContext, VectorStoreIndex
+from llama_index.core import StorageContext, VectorStoreIndex, SimpleDirectoryReader, load_index_from_storage
 from llama_index.embeddings.gemini import GeminiEmbedding
 from llama_index.vector_stores.chroma import ChromaVectorStore
 
@@ -13,27 +13,38 @@ class ContextualRetrieval:
 
     
     def generate_embeddings(self):
+        # Initialize Chromadb llamaindex wrapper class and Gemini embedding model
+        chroma_vector_store = ChromaVectorStore(chroma_collection=collection)
         embed_model = GeminiEmbedding(model_name="models/embedding-001", 
                                       api_key=os.environ.get("GOOGLE_API_KEY"),
                                       title="Apple Q3 2024 Financial Statement")
-        embeddings = embed_model.get_text_embedding
+        # Get the documents ready to be loaded
+        documents = SimpleDirectoryReader("data").load_data()
+        storage_context = StorageContext.from_defaults(vector_store=chroma_vector_store)
+         # Load everhything
+        index = VectorStoreIndex.from_documents(documents=documents, embed_model=embed_model, storage_context=storage_context)
+        
+    def query_data(self, query):
+        storage_context = StorageContext.from_defaults(persist_dir="/tmp")
+        loaded_index = load_index_from_storage(storage_context=storage_context)
+        query_engine = loaded_index.as_query_engine()
+        return query_engine.query("Earnings before taxes")
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":                                                                  
     load_dotenv()
-    chromadb_client = chromadb.EphemeralClient()
+    # Initializations
+    chromadb_client = chromadb.PersistentClient()
     anthropic_client = anthropic.Anthropic()
     collection = chromadb_client.create_collection(name="embeddings")
-    chroma_vector_store = ChromaVectorStore(chroma_collection=collection)
-    index = Vec
-    contextual_retrieval = ContextualRetrieval(anthropic_client=anthropic_client, chroma_collection=collection)
+    contextual_retrieval = ContextualRetrieval(anthropic_client=anthropic_client, 
+                                               chroma_collection=collection)
+    contextual_retrieval.generate_embeddings()
+    contextual_retrieval.query_data
 
 
 
 
-        
 
 
-
-
-    
+                                                                                                                                                                                                                           
